ea300bc804a08a1c9f2ea39416e829c40c54323b
陈志谦 <761208@nd.com>
2019-08-02 21:05:20 +0800

    archive output to zip with version

---
+++ src/activ-cast/locales/archiver.ts
@@ -0,0 +1,48 @@
+const path = require('path');
+var fs = require('fs');
+var archiver = require('archiver');
+
+class Archiver {
+    constructor() {
+        var manifest = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../../activ-cast/manifest.json")));
+        var source = "activ-cast"
+        var inputDir = path.resolve(__dirname , "../../../dist", source);
+        var outputFile = path.resolve(__dirname , "../../../dist", source + "-" + manifest.version  + ".zip")
+        var output = fs.createWriteStream(outputFile);
+        var archive = archiver('zip', {
+            zlib: { level: 9 } // Sets the compression level.
+        });
+
+        output.on('close', function() {
+            console.log(archive.pointer() + ' total bytes:', outputFile);
+            console.log('archiver has been finalized and the output file descriptor has closed.');
+        });
+
+        output.on('end', function() {
+            console.log('Data has been drained');
+        });
+
+        // good practice to catch warnings (ie stat failures and other non-blocking errors)
+        archive.on('warning', function(err) {
+            if (err.code === 'ENOENT') {
+              // log warning
+            } else {
+              // throw error
+              throw err;
+            }
+        });     
+        
+        // good practice to catch this error explicitly
+        archive.on('error', function(err) {
+            throw err;
+        });
+
+        archive.pipe(output);
+
+        // append files from a sub-directory and naming it `new-subdir` within the archive
+        archive.directory(inputDir, source);
+        archive.finalize();
+    }
+}
+
+new Archiver();
